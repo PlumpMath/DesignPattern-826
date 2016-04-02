449d4005e11bbb17e2cda71e02c543224a436294
George.Chou <shizhe.zhou@gmail.com>
2016-04-02 12:24:54 +0800

    Composite Pattern

---
+++ Design_Pattern/src/designpatterns/composite/menuiterator/CompositeIterator.java
@@ -0,0 +1,45 @@
+package designpatterns.composite.menuiterator;
+
+import java.util.*;
+
+public class CompositeIterator implements Iterator<MenuComponent> {
+	Stack<Iterator<MenuComponent>> stack = new Stack<Iterator<MenuComponent>>();
+
+	public CompositeIterator(Iterator<MenuComponent> iterator) {
+		stack.push(iterator);
+
+	}
+
+	@Override
+	public boolean hasNext() {
+		if (stack.empty())
+			return false;
+		else {
+			Iterator<MenuComponent> iterator = stack.peek();
+			if (!iterator.hasNext()) {
+				stack.pop();
+				return hasNext();
+			} else {
+				return true;
+			}
+		}
+	}
+
+	@Override
+	public MenuComponent next() {
+		if (hasNext()) {
+			Iterator<MenuComponent> iterator = stack.peek();
+			MenuComponent component = iterator.next();
+			stack.push(component.createIterator());
+			return component;
+		} else {
+			return null;
+		}
+	}
+
+	@Override
+	public void remove() {
+		throw new UnsupportedOperationException();
+	}
+
+}
